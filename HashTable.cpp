f105b459460ad477c4561545fc8a139d0b5f509f
Josue Rosales <jrosa1756@gmail.com>
2019-11-20 16:39:03 -0600

    Added hash function

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,7 +1,6 @@
 #include "HashTable.h"
 
 ///////////////////// TODO: FILL OUT THE FUNCTIONS /////////////////////
-
 // default constructor
 HashTable::HashTable() {
 
@@ -13,8 +12,12 @@ HashTable::~HashTable() {
 
 // hash function to determine index where string key goes, as mentioned in the instructions
 int HashTable::hash(std::string s) {
-
-	return 0;
+	int hash = 0;
+	int n = s.length();
+	for(int i = 0; i < n; i++) {
+		hash = g * hash + s.at(i);
+	}
+	return hash % 101;
 }
 
 // returns a boolean of whether the hash table is empty or not
