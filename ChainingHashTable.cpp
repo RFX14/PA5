99e1744c7286f7b0611f720c35d32773536bb250
Josue Rosales <jrosa1756@gmail.com>
2019-11-21 13:56:28 -0600

    Starting insert function

--- ChainingHashTable.cpp
+++ ChainingHashTable.cpp
@@ -4,7 +4,9 @@
 
 // constructor (NOTE: graders will use a default constructor for testing)
 ChainingHashTable::ChainingHashTable(std::string str) : HashTable(str) {
-	
+	for(int i = 0; i < data.size(); i++) {
+		insert(data.at(i), HashTable::hash(data.at(i)));
+	}
 }
 
 // destructor
@@ -14,7 +16,7 @@ ChainingHashTable::~ChainingHashTable() {
 
 // inserts the given string key
 void ChainingHashTable::insert(std::string key, int val) {
-
+	
 }
 
 // removes the given key from the hash table - if the key is not in the list, throw an error
